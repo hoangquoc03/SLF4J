63f24774ad8799d7c6e1a9fdde599b6e9a7ef03e
lequoc <lequoc150305@quoc150305gmail.com>
2026-06-01 18:52:56 +0700

    Truy vết yêu cầu với MDC (Mapped Diagnostic Context)

--- src/main/java/org/example/slf4j/Service/PatientService.java
+++ src/main/java/org/example/slf4j/Service/PatientService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-public class PatientService {
+public class PatientService   {
     public void searchPatient(){
         log.trace("Start searching patient");
         log.info("Searching patient...");
