003397b49133e7d62a2dd0a3ec93bfa18013b3cc
lequoc <lequoc150305@quoc150305gmail.com>
2026-06-01 16:57:08 +0700

    Quản lý Cấp độ Log và Cấu hình Console

---
+++ src/main/java/org/example/slf4j/Service/PatientService.java
@@ -0,0 +1,14 @@
+package org.example.slf4j.Service;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class PatientService {
+    public void searchPatient(){
+        log.trace("Start searching patient");
+        log.info("Searching patient...");
+    }
+}
